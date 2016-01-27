ba4c457a3d64307984d355756352da98e8ba6e9a
pdpatel2 <pdpatel2@gmail.com>
2016-01-26 23:40:12 -0500

    project setup

---
+++ app.js
@@ -0,0 +1,39 @@
+
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var swig = require('swig');
+var routes = require('./routes/index');
+
+// Incorporates swig into its rendering
+// point res.render to the proper directory
+app.set('views', __dirname + '/views');
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use swig to do so
+app.engine('html', swig.renderFile);
+// turn off swig's caching
+swig.setDefaults({cache: false});
+
+// Logs information about each incoming request
+app.use(morgan('dev'));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Parse application/json
+app.use(bodyParser.json())
+
+// Routes requests
+app.use('/', routes)
+
+// Serves up static files from some kind of public folder
+app.use(express.static('public'));
+
+// // Listen to server
+app.listen(3000, function(){
+	console.log("Listening to server")
+})
